57a32c8737888d79f4f8978bee1fca41e6cc5a4c
iwasiu <iwasiu460@gmail.com>
2021-03-02 17:09:47 +0100

    Added a method to generate uniqueID for each student.

--- StudentDatabaseApp/src/studentDatabaseApp/Student.java
+++ StudentDatabaseApp/src/studentDatabaseApp/Student.java
@@ -6,11 +6,12 @@ public class Student {
 	private String firstname;
 	private String lastname;
 	private int yearLevel;
-	private int studentID;
+	private String studentID;
 	private int noOfStudent;
 	private String course;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	private static int increment = 1000;
 	
 	// constructor that prompt user to enter first and last names and grade year
 	public Student() {
@@ -24,10 +25,24 @@ public class Student {
 		System.out.println("Enter grade year:\n1 - Gold\n2 - Silver\n3 - Diamond\n4 - Bronze");
 		this.yearLevel = sc.nextInt();
 		
+		//call method "setStudentID"
+		setStudentID();
+		
+		// Display student's info
 		System.out.println("Name: "+this.lastname+" "+this.firstname+" "
-		+"\nGrade Year: "+this.yearLevel);
+		+"\nGrade Year: "+this.yearLevel
+		+"\nStudent UniqueID: "+this.studentID);
+	
+		
 		
 	}
+	
+	// set student's uniqueID i.e. levelYear|increment(5 Digit)
+	private void setStudentID() {
+		++increment;
+		studentID = this.yearLevel+""+increment;
+	}
+	
 	
 	
 }
